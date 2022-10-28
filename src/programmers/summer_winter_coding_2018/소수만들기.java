83a68d427fc155bbbc2b9871e702ed6cda3b80c0
Dave-nw <conak4627@gmail.com>
2022-10-28 15:23:17 +0900

    프로그래머스 Summer/Winter Coding(~2018) 소수 만들기 완료

---
+++ src/programmers/summer_winter_coding_2018/소수만들기.java
@@ -0,0 +1,27 @@
+package programmers.summer_winter_coding_2018;
+
+public class 소수만들기 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    private boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
